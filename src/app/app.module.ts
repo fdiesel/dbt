b8f724a05acfdf6ee6cab040cf601edc51e15ca4
Florian Diesel <florian.diesel@posteo.de>
2023-03-23 11:14:07 +0100

    feat: header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,25 +7,27 @@ import {ServiceWorkerModule} from '@angular/service-worker';
 import {UpdateModule} from "./shared/feature/update/update.module";
 import {NavbarModule} from "./shared/ui/navbar/navbar.module";
 import {FooterModule} from "./footer/feature/footer.module";
+import {HeaderModule} from "./header/feature/header.module";
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    }),
-    UpdateModule,
-    NavbarModule,
-    FooterModule,
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            // Register the ServiceWorker as soon as the application is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000'
+        }),
+        UpdateModule,
+        NavbarModule,
+        FooterModule,
+        HeaderModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
